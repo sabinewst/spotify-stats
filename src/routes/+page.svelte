1c51b6518be65938a3537bd2e05701b56c15f6eb
Sabine <58789290+sabinewst@users.noreply.github.com>
2023-09-22 11:47:38 +0200

    feat: update authorization (#1)
    * Update authorization flow
    * Add loading state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,40 +5,68 @@
 	import { onMount } from 'svelte';
 
 	export let data;
+	let loading = true;
 
-	const getDataFromUrl = () => {
-		return window.location.hash
-			.substring(1)
-			.split('&')
-			.reduce((initial: { [key: string]: string }, item: string) => {
-				const parts = item.split('=');
-				initial[parts[0]] = decodeURIComponent(parts[1]);
+	const getCodeAndRequestToken = () => {
+		const urlParams = new URLSearchParams(window.location.search);
+		let code = urlParams.get('code') || '';
+		let codeVerifier = localStorage.getItem('code_verifier') || '';
 
-				return initial;
-			}, {});
+		let body = new URLSearchParams({
+			grant_type: 'authorization_code',
+			code: code,
+			redirect_uri: data.redirectUri,
+			client_id: data.clientId,
+			code_verifier: codeVerifier
+		});
+
+		fetch('https://accounts.spotify.com/api/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: body
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('HTTP status ' + response.status);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				localStorage.setItem('access_token', data.access_token);
+				$token = data.access_token;
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
 	};
 
 	onMount(() => {
-		if (!$token) {
-			const spotifyData = getDataFromUrl();
-			if (spotifyData.access_token) {
-				token.set(spotifyData.access_token);
-			}
-
+		const localToken = localStorage.getItem('access_token');
+		if (!$token && localToken) {
+			$token = localToken;
+		}
+		if (!$token && localStorage.getItem('code_verifier')) {
+			getCodeAndRequestToken();
 			// reset url in browser window
 			window.history.pushState({}, '', data.redirectUri);
 		}
+		loading = false;
 	});
 </script>
 
-{#if !$token}
+{#if loading}
+	<p>Loading...</p>
+{/if}
+{#if !loading && !$token}
 	<p>
 		Don't want to wait until the end of the year to find out which artists and tracks you've been
 		listening to the most? You've come to the right place.
 	</p>
-
 	<Login clientId={data.clientId} redirectUri={data.redirectUri} />
-{:else}
+{/if}
+{#if !loading && $token}
 	<Stats />
 {/if}
 
